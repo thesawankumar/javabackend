13682b66ae050f70bbdc4d984d0c8a51a037aa38
Sawan Kumar <sawankushwaha222@gmail.com>
2025-07-04 19:42:44 +0530

    Rest Api completed

---
+++ journalApp/src/main/java/com/sawan/journalApp/JournalApplication.java
@@ -0,0 +1,13 @@
+package com.sawan.journalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalApplication.class, args);
+    }
+
+}
