0ec54460f4ee50a983d0c195744d3f5581e39d00
Sawan Kumar <sawankushwaha222@gmail.com>
2025-07-06 10:56:16 +0530

    Role Based Authentication

--- journalApp/src/main/java/com/sawan/journalApp/config/SpringSecurity.java
+++ journalApp/src/main/java/com/sawan/journalApp/config/SpringSecurity.java
@@ -29,7 +29,7 @@ public class SpringSecurity {
                 .authorizeHttpRequests(request -> request
 //                        .requestMatchers("/public/**").permitAll()
                         .requestMatchers("/journal/**", "/user/**").authenticated()
-//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll())
                 .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
--- journalApp/src/main/java/com/sawan/journalApp/service/UserService.java
+++ journalApp/src/main/java/com/sawan/journalApp/service/UserService.java
@@ -30,6 +30,12 @@ public class UserService {
         userRepository.save(user);
     }
 
+    public void saveAdmin(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER", "ADMIN"));
+        userRepository.save(user);
+    }
+
     public List<User> getAll() {
         return userRepository.findAll();
     }
