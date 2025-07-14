3f4cdb444af758223e0b2fc76eb2d5ab08c5671b
Sawan Kumar <sawankushwaha222@gmail.com>
2025-07-14 13:31:15 +0530

    Mastering MongoTemplate

---
+++ journalApp/src/main/java/com/sawan/journalApp/repository/UserRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.sawan.journalApp.repository;
+
+import com.sawan.journalApp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+public class UserRepositoryImpl {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public List<User> getUserForSA() {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z|A-Z]{2,10}$"));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+        List<User> users = mongoTemplate.find(query, User.class);
+        return users;
+
+    }
+}
